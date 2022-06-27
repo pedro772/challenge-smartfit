560d7b78c9f66bc4988325cc328ffae310d5999f
pedro772 <plucena17230@gmail.com>
2022-06-26 23:45:02 -0300

    Obter dados das unidades via Fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react"
 import { Footer } from "./components/Footer"
 import { Form } from "./components/Form"
 import { Header } from "./components/Header"
 import { Labels } from "./components/Labels"
 
+const apiURL = "https://test-frontend-developer.s3.amazonaws.com/data/locations.json"
+
 function App() {
+  const [unitData, setUnitData] = useState({});
+
+  useEffect(() => {
+    getUnits();
+  }, []);
+
+  const getUnits = async () => {
+    const res = await fetch(apiURL);
+    const jsonData = await res.json();
+    setUnitData(jsonData);
+  }
 
   return (
     <div className="flex flex-col min-h-screen">
